cb5f5ccc6f0b15647de98f59e5636e1b4c0209bb
youngseo1104 <dudtj3116@naver.com>
2025-08-28 11:09:15 +0900

    to do list 기능

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 let addButton = document.getElementById('add-btn');
 let taskInput = document.getElementById('task-input');
-let testArea = document.getElementsByClassName('task');
 let taskList = [];
+let tabs = document.querySelectorAll('.task-tab li');
+let mode = 'all';
+let filterList = [];
 
 addButton.addEventListener('click', addTask);
 taskInput.addEventListener('keypress', keyPress);
+
+for (let i=0; i<tabs.length; i++) {
+  tabs[i].addEventListener('click', function(e){filter(e)});
+}
 
 function addTask() {
   let task = {
@@ -12,30 +18,39 @@ function addTask() {
     taskContent: taskInput.value,
     isComplete: false
   }
-  taskList.push(task);
-  render();
-  taskInput.value = '';
-
-  console.log('taskList:', taskList);
+  if (taskInput.value == '' || taskInput.value == null) {
+    alert('할일을 적어주세요');
+  } else {
+    taskList.push(task);
+    filter();
+    taskInput.value = '';
+  }
 }
 
 function render() {
+  let list = [];
+  if (mode === 'all') {
+    list = taskList;
+  } else {
+    list = filterList;
+  }
+  
   let resultHtml = '';
-  for (let i=0; i < taskList.length; i++) {
-    if (taskList[i].isComplete == true) {
-      resultHtml += `<div class="task">
-        <div class='task-done'>${taskList[i].taskContent}</div>
+  for (let i=0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
+      resultHtml += `<div class="task t-done">
+        <div class='task-done'>${list[i].taskContent}</div>
         <div>
-          <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-          <button onclick="deleteTask()">Delete</button>
+          <button onclick="toggleComplete('${list[i].id}')" class='check-btn'>Check</button>
+          <button onclick="deleteTask('${list[i].id}')" class='delete-btn'>❌</button>
         </div>
       </div>`;
     } else {
       resultHtml += `<div class="task">
-        <div>${taskList[i].taskContent}</div>
+        <div>${list[i].taskContent}</div>
         <div>
-          <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-          <button onclick="deleteTask()">Delete</button>
+          <button onclick="toggleComplete('${list[i].id}')" class='check-btn'>Check</button>
+          <button onclick="deleteTask('${list[i].id}')" class='delete-btn'>❌</button>
         </div>
       </div>`;
     }
@@ -51,12 +66,48 @@ function toggleComplete(idCheck) {
       break;
     }
   }
-
-  render();
+  filter();
 }
 
-function deleteTask() {
-  console.log('삭제하자!');
+function deleteTask(idCheck) {
+  for(let i=0; i<taskList.length; i++) {
+    if (taskList[i].id == idCheck) {
+      taskList.splice(i, 1);
+    }
+  }
+  filter();
+}
+
+function filter(e) {
+  if (e) {
+    mode = e.target.id;
+
+    for (let i=0; i<tabs.length; i++) {
+      if (tabs[i] == e.target) {
+        tabs[i].classList.add('active');
+      } else {
+        tabs[i].classList.remove('active');
+      }
+    }
+  }
+  filterList = [];
+  if (mode === 'all') {
+    render();
+  } else if (mode === 'ongoing') { //진행중
+    for(let i=0; i<taskList.length; i++) {
+      if (taskList[i].isComplete === false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  } else { 
+    for(let i=0; i<taskList.length; i++) {
+      if (taskList[i].isComplete === true) {
+        filterList.push(taskList[i]);
+      }
+      render();
+    }
+  }
 }
 
 function randomIDGenerate() {
@@ -67,4 +118,8 @@ function keyPress(e) {
   if (e.key == 'Enter') {
     addTask();
   }
+}
+
+function tabTask(e) {
+  render();
 }
